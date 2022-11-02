c49c570e354099b4124685390b7d9a007f377de6
johannbb <johannbb@stud.ntnu.no>
2022-11-02 08:54:37 +0100

    Laget js funksjon så bilder forstørres ved hover

--- js1.js
+++ js1.js
@@ -1,6 +1,13 @@
-    // funksjon for å forstørre bilde ved klikk
+    // Function to increase image size
     function enlargeImg(img) {
         img.style.transform = "scale(1.5)";
         img.style.transition =
             "transform 0.25s ease";
     }
+
+    function resetImg(img) {
+        // Set image size to original
+        img.style.transform = "scale(1)";
+        img.style.transition = "transform 0.25s ease";
+      }
+
