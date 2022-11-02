10cca91433ce4cb555434e23446d8d8fccb09049
johannbb <johannbb@stud.ntnu.no>
2022-11-02 09:46:54 +0100

    Lagt inn kommentarer Ãpå js1

--- js1.js
+++ js1.js
@@ -1,12 +1,11 @@
-    // Function to increase image size
+    // Funksjon for å forstørre bilde
     function enlargeImg(img) {
         img.style.transform = "scale(1.5)";
         img.style.transition =
             "transform 0.25s ease";
     }
-
+    // Funksjon for å resete bildet tilbake til originalstørrelse
     function resetImg(img) {
-        // Set image size to original
         img.style.transform = "scale(1)";
         img.style.transition = "transform 0.25s ease";
       }
