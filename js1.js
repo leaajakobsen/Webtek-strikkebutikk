dbd9460008829cc030d1532cf26958ffb1a08a4f
malingun <malin9@live.no>
2022-11-05 08:30:43 +0100

    Kontaktsskjema.html Laget kontaktskjema Fikset js for kontaktskjema Fikset hjem-knapp i overskriften

--- js1.js
+++ js1.js
@@ -8,5 +8,12 @@
     function resetImg(img) {
         img.style.transform = "scale(1)";
         img.style.transition = "transform 0.25s ease";
-      }
+    }
+
+    // Funksjon for å resete kontaktskjema
+    function resetForm() {
+        document.getElementById("Kontaktskjema").onsubmit();
+        document.getElementById("kontaktskjema").reset();
+    }
+
 
