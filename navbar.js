ae733b8cc682713860a308c25fffe9d5fcb50035
johannbb <johannbb@stud.ntnu.no>
2022-11-08 22:02:58 +0100

    prøvde å lage js til footer

--- navbar.js
+++ navbar.js
@@ -26,3 +26,18 @@ for (let i = 0; i<links.length; i++) {
 
     navbar.appendChild(a)
 }
+
+/*
+const footer = document.getElementById("footer")
+const text = [
+    {label : "Adresse: Strikkegata 26, Trondheim",},
+    {label : "Tlf: +47 123 45 678",},
+    {label : "Mail: knit@happens.com",},
+    {label : "Følg oss på instagram! @knit.happ",},
+
+]
+
+for (let i = 0; i<text.length; i++){
+    let content = document.createTextNode(text[i].label)
+    footer.appendChild(content)
+}*/
