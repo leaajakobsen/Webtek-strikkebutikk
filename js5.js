c3f84ee5de96881a3081efd39e3032e671e725d8
johannbb <johannbb@stud.ntnu.no>
2022-11-12 14:57:31 +0100

    Endret litt i css og laget changes html

--- js5.js
+++ js5.js
@@ -1,12 +1,13 @@
 /* h1 endrer farge onmouseover, av Lise*/
 
-const tittel = document.getElementById("header")
+const tittel = document.getElementById('header')
 
 tittel.addEventListener('mouseover', function changeColor(){
     tittel.style.color ='hsl(196, 48%, 74%)'
-
 });
 
 tittel.addEventListener('mouseout', function changeColor(){
     tittel.style.color = 'black'
 });
+
+
