693f5ecc9dae9699fab513e0015b8d484cd0e21b
johannbb <johannbb@stud.ntnu.no>
2022-11-14 08:22:25 +0100

    Linket til hjemmesiden i header

--- components.js
+++ components.js
@@ -1,7 +1,7 @@
 //Funksjon for å lage header
 function makeh1() {
     let a = document.createElement("a")
-    a.setAttribute("href","index.html")
+    a.setAttribute("href","hjemmeside.html")
 
      const header = document.createElement('h1');
      const text = document.createTextNode("KNIT HAPPENS");
